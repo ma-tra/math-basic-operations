e7494289d332696cc58262477300eca72bed4f29
Unknown <m.traninger@aon.at>
2020-11-09 14:54:06 +0100

    Adds test script to for CI to repo

---
+++ test_core.py
@@ -0,0 +1,17 @@
+from core import calculator
+from core import is_prime
+from core import add_frac
+
+def test_calculator():
+	assert calculator(2,2) == [4,0,1,4]
+
+
+def test_is_prime():
+    
+    primes = [1,2,3,5,13,7,59,97]
+    for prime in primes:
+        assert is_prime(prime) == True
+    assert is_prime(8) == False
+    
+def test_add_frac():
+	assert add_frac(1,2,1,4) == [6,8]
